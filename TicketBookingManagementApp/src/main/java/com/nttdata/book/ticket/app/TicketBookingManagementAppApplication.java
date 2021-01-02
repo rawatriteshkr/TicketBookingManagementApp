e4d4351fb4f58ccc3f5c6bed381478d54b4eee72
admin <admin@DESKTOP-82POU4M>
2021-01-02 14:34:07 +0530

    used CommandLineRunner interface

--- TicketBookingManagementApp/src/main/java/com/nttdata/book/ticket/app/TicketBookingManagementAppApplication.java
+++ TicketBookingManagementApp/src/main/java/com/nttdata/book/ticket/app/TicketBookingManagementAppApplication.java
@@ -2,22 +2,33 @@ package com.nttdata.book.ticket.app;
 
 import java.util.Date;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 import com.nttdata.book.ticket.app.entities.Ticket;
 import com.nttdata.book.ticket.app.service.TicketBookingService;
 
 @SpringBootApplication
-public class TicketBookingManagementAppApplication {
+public class TicketBookingManagementAppApplication implements CommandLineRunner {
+	
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private TicketBookingService ticketBookingService;
 
 	public static void main(String[] args) {
 		
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(TicketBookingManagementAppApplication.class, args);
+	 SpringApplication.run(TicketBookingManagementAppApplication.class, args);
 		
-		TicketBookingService ticketBookingService = applicationContext.getBean("ticketBookingService", TicketBookingService.class);
 		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		Ticket ticket = new Ticket();
 		ticket.setBookingDate(new Date());
 		ticket.setDestStation("Mumbai");
@@ -27,6 +38,7 @@ public class TicketBookingManagementAppApplication {
 		
 		Ticket t = ticketBookingService.createTicket(ticket);
 		System.out.println("Ticket Id : "+t.getTicketId());
+		System.out.println("DataSource "+dataSource.getClass());
 	}
 
 }
